64790609ad67fea56c2f942577d82a1e8115431d
Twlucas <rafa_lucasrafael@hotmail.com>
2016-06-26 16:17:33 -0300

    DAO Recibo
    Criado um DAO para o recibo.

---
+++ Implementação/OrtodontKit/src/br/com/DAO/DAORecibo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.DAO;
+
+import br.com.model.bd.Recibo;
+
+/**
+ *
+ * @author Lucas
+ */
+public class DAORecibo extends DAOGenerico<Recibo>{
+    
+    public DAORecibo() {
+        super(Recibo.class);
+    }  
+}
+
